cdc2018235501271395a32d355297888dc0c1796
kerumandev <francoxmelchor@gmail.com>
2025-10-04 23:52:30 -0500

    main nuevo landinpage

--- facefind_front/src/views/LandingPage.jsx
+++ facefind_front/src/views/LandingPage.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Statistics from './components/Statistics';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
-import UserCasesView from './views/UserCasesView';
-import './App.css';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Statistics from '../components/Statistics';
+import ContactForm from '../components/ContactForm';
+import Footer from '../components/Footer';
 
-function App() {
+const LandingPage = () => {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={
-            <main>
-              <Hero />
-              <About />
-              <Statistics />
-              <ContactForm />
-            </main>
-          } />
-          <Route path="/cases" element={<UserCasesView />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="landing-page">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Statistics />
+        <ContactForm />
+      </main>
+      <Footer />
+    </div>
   );
-}
+};
 
-export default App;
+export default LandingPage;
